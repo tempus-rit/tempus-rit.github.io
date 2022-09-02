154364e4dc18e3998ec171ef31f9e0048e23f8bd
Raavi Chowdhury <rkc3356@g.rit.edu>
2022-09-02 00:00:44 -0400

    Added initial page and logo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import tempus_logo from './media/TEMPUS.jpg'
+import { Container, Row, Col } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img id={"logo"} src={tempus_logo} alt={"TEMPUS LOGOS"} />
+        <Container>
+          <Row md={3}>
+            <Col>
+              <div className={"TeamMembers"}>
+                <ul>
+                  <li>Project Manager: Ethan Yaccarino-Mims</li>
+                  <li>Communications Manager: Raavi Chowdhury</li>
+                  <li>Johann Lee</li>
+                  <li>Bradley Boxer</li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+
+
       </header>
     </div>
   );

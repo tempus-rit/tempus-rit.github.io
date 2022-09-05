9c897e91b87d51207adb2719ec4e38e46b848893
Bradley Boxer <brad.boxer1@gmail.com>
2022-09-05 13:12:54 -0400

    homepage progress

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import tempusLogo from '../../media/TEMPUS.jpg';
+import oroliaLogo from '../../media/orolia-logo.svg'
 import styled from 'styled-components';
 
 const Page = styled.div`
@@ -7,25 +8,107 @@ const Page = styled.div`
   height: 100%;
   display: grid;
   place-items: center;
-  grid-template-rows: 3fr 1fr;
+  grid-template-rows: 1fr 1.5fr;
 `;
+
+const Sponsor = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding-left: 20px;
+`;
+
+const Text = styled.p`
+  padding-top: 5px;
+`;
+
+const TwoColumn = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  place-self: start center;
+  justify-content: space-evenly;
+`;
+
+const TempusLogo = () => <img style={{ maxWidth: 500, display: 'block', margin: 'auto' }} src={tempusLogo} alt='tempus logo'/>
+const OroliaLogo = () => <img style={{ width: 120, margin: 10 }} src={oroliaLogo} alt='orolia'/>;
 
 const HomePage = () => {
   return (
     <Page>
       <div>
-        <img id='logo' src={tempusLogo} alt='tempus logo' />
-        <div>
-          <ul>
-            <li>Project Manager: Ethan Yaccarino-Mims</li>
-            <li>Communications Manager: Raavi Chowdhury</li>
-            <li>Johann Lee</li>
-            <li>Bradley Boxer</li>
-          </ul>
-        </div>
+        <TempusLogo />
+        <h3 style={{ marginBottom: 0 }}>Timing System Central Configuration</h3>
+        <Sponsor>
+          <Text>Sponsored by</Text>
+          <OroliaLogo />
+          <Text>with support from <b>Xueling Zhang</b></Text>
+        </Sponsor>
       </div>
+      <TwoColumn>
+        <div style={{ width: '40%', minWidth: 500 }}>
+          <h3>About</h3>
+          <p style={{ marginLeft: 10 }}>
+            A description of the project goes here. It should be no longer than 250 words, and should be approved by the sponsor and coach.
+          </p>
+        </div>
+        <div>
+          <h3>Team Members</h3>
+          <MemberTable />
+        </div>
+      </TwoColumn>
     </Page>
   );
 };
+
+const Table = styled.table`
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+
+  & th, & td {
+    padding: 6px 7px;
+  }
+
+  & thead tr {
+    background-color: #245cac;
+    color: white;
+  }
+  
+  & tbody tr:nth-of-type(even) {
+    background-color: #f3f3f3;
+  }
+  
+  & tbody tr:last-of-type {
+    border-bottom: 2px solid #009879;
+  }
+`;
+
+const MemberTable = () => <Table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Role</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Ethan Yaccarino-Mims</td>
+      <td>Project Manager</td>
+    </tr>
+    <tr>
+      <td>Raavi Chowdhury</td>
+      <td>Communications Manager</td>
+    </tr>
+    <tr>
+      <td>Johann Lee</td>
+      <td />
+    </tr>
+    <tr>
+      <td>Bradley Boxer</td>
+      <td />
+    </tr>
+  </tbody>
+</Table>
 
 export default HomePage;
